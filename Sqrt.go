75a98809d9a42b8ef5e90c6fe42daf446dfb594a
Igor Tchoursinov <i.tchoursinov@gmail.com>
2022-01-12 17:46:05 +0300

    fix algorythm & modify output

--- Sqrt.go
+++ Sqrt.go
@@ -5,21 +5,11 @@ import (
 )
 
 func Sqrt(x float64) float64 {
-	lim := 2.0
-	var result float64
+	lim := 1.0
 	for i := 0; i < 10; i++ {
-		var res = (lim + (x / lim)) / 2
-		//fmt.Println("res = ", res)
-		//fmt.Println(lim/res)
-		if lim/res > 1 {
-			lim = res
-		} else {
-			result = res
-			return result
-		}
-		result = res
+		lim -= (lim*lim - x) / (2 * lim)
 	}
-	return result
+	return lim
 }
 
 func Pow(x float64) float64 {
@@ -31,8 +21,8 @@ func main() {
 	var num int
 	var _, err = fmt.Scan(&num)
 	if err != nil {
-		fmt.Println("Ошибка ввода: ", err)
+		fmt.Printf("Ошибка ввода: %s", err)
 	} else {
-		fmt.Println(Pow(Sqrt(float64(num))))
+		fmt.Printf("Pow & Sqrt num %d - result %f", num, Pow(Sqrt(float64(num))))
 	}
 }
